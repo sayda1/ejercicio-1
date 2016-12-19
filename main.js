e0cea9f6b6590be65a7101b00ce3b0321f476e5b
sayda1 <saydaquintanilla1@gmail.com>
2016-12-19 17:14:55 -0500

    suma

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var numero1, numero2, numero3, resultado;
 
-numero1 = prompt("por fabor ,ingresa un numero");
-numero2 = prompt("por fabor ,ingresa otro numero");
-numero3 = prompt("por fabor ,ingresa tercer numero");
+numero1 = prompt("por favor ,ingresa un numero");
+numero2 = prompt("por favor ,ingresa otro numero");
+numero3 = prompt("por favor ,ingresa tercer numero");
 
-resultado = parseInt(numero1) * parseInt(numero2) - parseInt(numero3);
+resultado = parseInt(numero1) + parseInt(numero2) - parseInt(numero3);
 alert (resultado);
